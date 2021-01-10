5c81cf6380bb72a601ebab65c09a52f86f10e4d9
teacher <itstep.codeclub@gmail.com>
2021-01-10 03:13:42 +0200

    Добавлена версия проекта

--- flask_app.py
+++ flask_app.py
@@ -5,9 +5,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('home/index.html', data={
         'title': 'Главная'
     })
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
